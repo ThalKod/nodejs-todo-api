67bb9c3ba7ab5f7df309d13ea81b17fd35ebf966
ThalKod <32584079+ThalKod@users.noreply.github.com>
2018-01-27 11:13:34 -0500

    Set posting data test

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,31 @@ const request = require("supertest");
 
 const {app}   = require("../app");
 const Todo    = require("../models/todo");
+
+// beforeEach((finish) =>{
+//     Todo.remove({}).then(() => finish());
+// });
+
+describe("POST /todos", () =>{
+    it("Should create a new Todo", (finish) =>{
+        var text = "Testing the text field";
+        
+        request(app)
+            .post("/todos")
+            .send({text})   
+            .expect(200)
+            .expect((res) =>{
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) =>{
+                if(err){
+                   return finish(err);
+                }
+
+                Todo.findById(res.body._id).then((todo) =>{
+                    expect(todo._id).toEqual(todo._id);
+                    finish();
+                }).catch((err) => finish(err));
+            });
+    });
+});
