a04086b3756dcceed3e57bebc630e7192cd9afc1
ThalKod <tmarcelin@thal.tech>
2018-03-08 12:31:44 -0500

    Add DELETE routes

--- app.js
+++ app.js
@@ -53,6 +53,22 @@ app.get("/todos/:id", (req, res) =>{
 
 });
 
+app.delete("/todos/:id", (req, res) =>{
+    
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(req.params.id).then((rTodo) =>{
+        if(!rTodo){
+            return res.status(404).send();
+        }
+        res.send(rTodo);
+    }).catch((err) =>{
+        res.status(404).send();
+    });
+});
+
 app.listen("3000", () =>{
     console.log("Server Started");
 });
