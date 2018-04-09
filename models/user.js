59a323c549d7244b78a2a153de1daa7f87c36b7c
ThalKod <tmarcelin@thal.tech>
2018-04-09 11:52:02 -0400

    Hash user password

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const validator = require("validator");
 const mongoose = require("mongoose");
 const jwt       = require("jsonwebtoken");
 const _         = require("lodash");
+const bcrypt    = require("bcryptjs");
 
 var userSchema = new mongoose.Schema({
     email: {
@@ -74,6 +75,23 @@ userSchema.statics.findByToken = function(token){
         "tokens.access": "auth"
     });
 };
+
+userSchema.pre("save", function(next){
+    var user = this;
+
+    if(user.isModified("password")){
+
+        bcrypt.genSalt(10, (err, salt) =>{
+            bcrypt.hash(user.password, salt, (err, res) =>{
+                user.password = res;
+                next();
+            });
+        });
+        
+    } else{
+        next();
+    }
+});
 
 var User = mongoose.model("User", userSchema);
 
